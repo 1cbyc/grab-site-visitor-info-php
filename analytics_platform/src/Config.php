a0e96b39b4be95a5e5b85e9144bc2a27fd5a1f68
1cbyc <ei@nsisong.com>
2025-10-28 23:07:24 +0100

    Generate new secure API key

--- analytics_platform/src/Config.php
+++ analytics_platform/src/Config.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    "dashboard_api_key" => "62ac2c680d914d6c7dfedc4cd60676dfd91e25b5bde895e4276031226ca6e7f7",
+    "dashboard_api_key" => "21ae02d01f5748af938378df604413eb48ccdda6ffad2d1291d7759fc225dbd5",
     "abuseipdb_api_key" => "YOUR_ABUSEIPDB_API_KEY_HERE",
 ];
