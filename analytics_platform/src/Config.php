f2b43c31561f7b9bc2ffdd72f6496fba8e6b4ae1
1cbyc <ei@nsisong.com>
2025-10-28 21:45:25 +0100

    update dashboard api key to generated secure key

--- analytics_platform/src/Config.php
+++ analytics_platform/src/Config.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    "dashboard_api_key" => "SUPER_SECRET_API_KEY",
+    "dashboard_api_key" => "62ac2c680d914d6c7dfedc4cd60676dfd91e25b5bde895e4276031226ca6e7f7",
     "abuseipdb_api_key" => "YOUR_ABUSEIPDB_API_KEY_HERE",
 ];
